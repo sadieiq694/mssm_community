cb3a89c8ca548e525732c99d65ce3a54b524e589
Sadie Allen <sadie.la418@gmail.com>
2017-05-10 09:25:39 -0400

    lists people

--- index.js
+++ index.js
@@ -10,15 +10,21 @@ var mysql      = require('mysql');
 app.set('views', './views') //pug templates are in the views folder
 app.set('view engine', 'pug') //use pug to render views
 
-function addPerson(name, role) {
+function create_connection() {
   var connection = mysql.createConnection({ //creating connection to the database
     host     : 'localhost',
     user     : 'sadie.la',
     password : '12345',
     database : 'mssm_community_2'
   });
-  connection.connect();
 
+  return connection;
+}
+
+function addPerson(name, role) {
+
+  connection = create_connection();
+  connection.connect();
   connection.query('INSERT INTO people SET ?', {name: name}, function (error, results, fields) { //name = column name, name = value for field
     if (error) throw error;
 
@@ -43,8 +49,29 @@ function addPerson(name, role) {
   });
 
 //connection.end();
-
 }
+
+function people_roles(callback) {
+  connection = create_connection();
+  connection.connect();
+  connection.query('SELECT people.name, roles.name as rolename FROM `people` INNER JOIN `user_roles` on people.id = user_roles.person_id INNER JOIN `roles` on user_roles.role_id = roles.role_id', {}, function(error, results, fields) {
+    if(error) throw error;
+
+    connection.end()
+    callback(results);
+  });
+  //return
+}
+
+
+app.get('/people', function(req, res) {
+  people_roles(function(results) {
+    //console.log(results);
+
+    res.render('people', {title: "People and Roles", people: results})
+  });
+});
+
 
 app.post('/added-person', function(req, res) {
   var name = req.body.user_name;
@@ -57,20 +84,17 @@ app.post('/added-person', function(req, res) {
   res.render('added-person')
 
 });
-
-
 /*connection.query('SELECT * FROM `people` JOIN `sign_out` ON people.id = sign_out.student_id', function (error, results, fields) {
   if (error) throw error;
   for(i = 0; i < results.length;  i++){
     console.log('The solution is: ', results[i]);
   }
 });*/
-
 //onnection.end();
 
 app.get('/test', function(req, res) {
   res.render('test', {title: 'Hey', message: "Hello there!"}) // 'test' is the name of a template, then fill fields (object w/ properties)
-})
+});
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /*app.get('/', function (req, res) { //this is sent to the webpage, configures webserver
