a8b89bd6f59b3f5e35c01c2b9b73b4b93a41e3f0
Sadie Allen <sadie.la418@gmail.com>
2017-05-21 16:41:12 -0400

    new homescreens

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express') //importing library
 var app = express() //app is an object that gives us access to commands in the express library
 var bodyParser = require('body-parser')
+var session = require('express-session') //store data specific to the person on the website
 app.use(bodyParser.json()); //to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({ //to suport URL-encoded bodies
   extended:true
@@ -9,68 +10,114 @@ app.use(bodyParser.urlencoded({ //to suport URL-encoded bodies
 var db = require('./dbinteract.js');
 
 
-app.set('views', './views') //pug templates are in the views folder
-app.set('view engine', 'pug') //use pug to render views
+app.set('views', './views'); //pug templates are in the views folder
+app.set('view engine', 'pug'); //use pug to render views
 
 
-
-////////////////////////// GETS /////////////////////////////////
+////////////////////////// GETS (ROOTS) /////////////////////////////////
+app.use(session({ secret: 'keyboard cat', cookie: { maxAge: 60000 }}));
 
 app.get('/people', function(req, res) {
-  db.people_roles(function(results) {
-    //console.log(results);
-
-    res.render('people', {title: "People and Roles", people: results})
-  });
+  var sess = req.session;
+  if(sess.user) {
+    db.people_roles(function(results) {
+      //console.log(results);
+      res.render('people', {title: "People and Roles", people: results});
+        });
+  }
+  else {
+      res.redirect('sign-in');
+  }
 });
 
 app.get('/events', function(req, res) { //EDITEDITEDITEDIT
-  db.list_events(function(results) {
-    //console.log(results);
+  var sess = req.session;
+  if(sess.user) {
+    db.list_events(function(results) {
+      //console.log(results);
 
-    res.render('events', {title: "Event List", events: results})
-  });
+      res.render('events', {title: "Event List", events: results});
+    });
+  }
+  else {
+      res.redirect('sign-in');
+  }
 });
 
 app.get('/add-event', function(req, res) {
-  db.chaperone_roles(function(results) {
-    //console.log(results);
+  var sess = req.session;
+  if(sess.user) {
+    db.chaperone_roles(function(results) {
+      //console.log(results);
 
-    res.render('add_event', {title: "Adding Events", chaperones: results})
-  });
+      res.render('add_event', {title: "Adding Events", chaperones: results})
+    });
+  }
+  else {
+      res.redirect('sign-in');
+  }
 });
 
 app.get('/sign-in', function(req, res) {
+  var sess = req.session
   db.people_roles(function(results) {
+    //sess.user = ??
     res.render('sign_in', {title: 'signin', people: results})
   });
 });
 
 app.get('/add-person', function(req, res) {
-  res.render('add_person', {title: 'Hey', message: "Hello there!"}) // 'test' is the name of a template, then fill fields (object w/ properties)
+  var sess = req.session
+  if(sess.user) {
+    res.render('add_person', {title: 'Hey', message: "Hello there!"}) // 'test' is the name of a template, then fill fields (object w/ properties)
+  }
+  else {
+      res.redirect('sign-in');
+  }
 });
 
 
 app.get('/home-ri', function(req, res){
-  res.render('home-ri', {title: 'RI Home', message: 'Hey!'})
+  var sess = req.session
+  if(sess.user) {
+    res.render('home_ri', {title: 'RI Home', message: 'Hey!'})
+  }
+  else {
+      res.redirect('sign-in');
+  }
 });
 
 app.get('/home-student', function(req, res){
-  res.render('home-student', {title: 'Student Home', message: 'Hey!'})
+  var sess = req.session
+  if(sess.user) {
+    res.render('home_student', {title: 'Student Home', message: 'Hey!'})
+  }
+  else {
+      res.redirect('sign-in');
+  }
 });
 
 app.get('/home-teacher', function(req, res){
-  res.render('home-student', {title: 'Teacher Home', message: 'Hey!'})
+  var sess = req.session
+  if(sess.user) {
+    res.render('home_student', {title: 'Teacher Home', message: 'Hey!'})
+  }
+  else {
+      res.redirect('sign-in');
+  }
 });
 
 ///////////////////// POST ///////////////////////////
 
 app.post('/signed-in', function(req, res) {
-  var user = req.body.user
+  var user = req.body.user;
+  var sess = req.session;
   console.log(user)
   db.person_info(user, function(results) {
     console.log(results);
+    sess.user = results;
     if(results.roleid == 1) {
+    /// REDIRECT IF WRONG ROLE!
       res.redirect('home-student'); //students: studenthome
     }
     else if (results.roleid==2) { //RI: reshome
@@ -81,7 +128,6 @@ app.post('/signed-in', function(req, res) {
     }
 
   });
-  //decide what page to go to based on role
 })
 
 app.post('/added-person', function(req, res) {
